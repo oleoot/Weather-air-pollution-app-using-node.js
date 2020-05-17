b29ca363275a565646d2854df7b822d954c18f49
oleoot <oleoot28@gmail.com>
2020-05-17 17:10:13 +0300

    Changed port for app deployment

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const Datastore = require('nedb');
 const app = express();
 const fetch = require('node-fetch');
+const port = process.env.PORT || 3000
 require('dotenv').config();
-app.listen(3000, () => console.log('Listening at 3000'))
+app.listen(port, () => console.log(`Starting server at ${port}`))
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }))
 
